12f6e8923b416f1174cee56b5c154b7d48d173ef
alskzldud <98021794+alskzldud@users.noreply.github.com>
2022-10-03 16:30:53 +0900

    카메라 이동

--- Dission3T/Assets/Player/Scripts/PlayerCtrl.cs
+++ Dission3T/Assets/Player/Scripts/PlayerCtrl.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerCtrl : MonoBehaviour
 {
+    public float speed = 100;
+
     void Start()
     {
         
@@ -11,6 +13,11 @@ public class PlayerCtrl : MonoBehaviour
 
     void Update()
     {
+        float rotX = Input.GetAxis("Mouse X"); //마우스 수평 이동
+
+        float degX = rotX * speed * Time.deltaTime; //회전 각도
+        transform.Rotate(0, degX, 0); //x축 기준으로 degY만큼 y축 기준으로 degX만큼 회전
+
         if (Input.GetKey(KeyCode.W))
         {
             transform.position += new Vector3(0.0f, 0.0f, 0.1f);
