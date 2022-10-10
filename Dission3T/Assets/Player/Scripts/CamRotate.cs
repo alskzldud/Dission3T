6eea537f4eb988dec03db27736f1a4b86085d1bb
alskzldud <98021794+alskzldud@users.noreply.github.com>
2022-10-10 20:34:29 +0900

    플레이어 이동, 시야 구현

---
+++ Dission3T/Assets/Player/Scripts/CamRotate.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamRotate : MonoBehaviour
+{
+    public float rotSpeed = 200f;
+    float mx = 0;
+    float my = 0;
+
+    void Update()
+    {
+        float x = Input.GetAxis("Mouse X"); //마우스 수평 이동
+        float y = Input.GetAxis("Mouse Y"); //마우스 수직 이동
+
+        mx += x * rotSpeed * Time.deltaTime;
+        my += y * rotSpeed * Time.deltaTime;
+
+        my = Mathf.Clamp(my, -45, 45); //시야 제한
+
+        transform.eulerAngles = new Vector3(-my, mx, 0); //카메라 회전
+
+    }
+}
