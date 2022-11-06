ce5f6e5db184adc2179af01ebadf5d9665e9b84b
alskzldud <98021794+alskzldud@users.noreply.github.com>
2022-11-07 00:49:43 +0900

    텍스트

--- Dission3T/Assets/Scripts/TextUI.cs
+++ Dission3T/Assets/Scripts/TextUI.cs
@@ -6,26 +6,38 @@ using UnityEngine;
 public class TextUI : MonoBehaviour
 {
     //패널 게임 오브젝트
-    public GameObject panel;
-
+    public GameObject Panel1;
+    //음료수냉장고
+    public GameObject Panel2;
 
     void OnTriggerEnter (Collider col)
     {
         //플레이어가 방문과 충돌하면
-        if (col.gameObject.tag == "Roomdoor")
+        if (col.gameObject.tag == "RoomDoor")
         {
             //Text UI 활성화
-            panel.SetActive(true);
+            Panel1.SetActive(true);
             //2초 후에 비활성화
             Invoke("False", 2.0f);
-            print("충돌");
+
+        }
+        
+        //플레이어가 음료수냉장고와 충돌하면
+        if (col.gameObject.tag == "DrinkR")
+        {
+            //Text UI 활성화
+            Panel2.SetActive(true);
+            //2초 후에 비활성화
+            Invoke("False", 2.0f);
         }
     }
 
     void False()
     {
         //Text UI 2초 후에 비활성화시키는 함수
-        panel.SetActive(false);
+        Panel1.SetActive(false);
+        //Text UI 2초 후에 비활성화시키는 함수
+        Panel2.SetActive(false);
     }
 }
 
