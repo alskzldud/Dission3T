3ffeb7cc935324fd1879017df0738de8cb16dfd1
alskzldud <98021794+alskzldud@users.noreply.github.com>
2022-11-19 22:39:38 +0900

    텍스트 ui 수정

--- Dission3T/Assets/Scripts/TextUI.cs
+++ Dission3T/Assets/Scripts/TextUI.cs
@@ -9,6 +9,8 @@ public class TextUI : MonoBehaviour
     public GameObject Panel1;
     //음료수냉장고
     public GameObject Panel2;
+    //널브러진 과자
+    public GameObject Panel3;
 
     void OnTriggerEnter (Collider col)
     {
@@ -30,6 +32,14 @@ public class TextUI : MonoBehaviour
             //2초 후에 비활성화
             Invoke("False", 2.0f);
         }
+        //플레이어가 과자와 충돌하면
+        if (col.gameObject.tag == "Snack")
+        {
+            //Text UI 활성화
+            Panel3.SetActive(true);
+            //2초 후에 비활성화
+            Invoke("False", 2.0f);
+        }
     }
 
     void False()
@@ -38,6 +48,8 @@ public class TextUI : MonoBehaviour
         Panel1.SetActive(false);
         //Text UI 2초 후에 비활성화시키는 함수
         Panel2.SetActive(false);
+        //Text UI 2초 후에 비활성화시키는 함수
+        Panel3.SetActive(false);
     }
 }
 
