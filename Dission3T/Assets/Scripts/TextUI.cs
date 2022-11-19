73e3381f9f1fe3e33b51af9a264f761ed21ae73d
alskzldud <98021794+alskzldud@users.noreply.github.com>
2022-11-19 23:32:38 +0900

    tv소리 추가

--- Dission3T/Assets/Scripts/TextUI.cs
+++ Dission3T/Assets/Scripts/TextUI.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 
 public class TextUI : MonoBehaviour
 {
-    //패널 게임 오브젝트
+    //Tv소리
     public GameObject Panel1;
+    //Tv소리 오디오 소스
+    public AudioSource tvsound;
     //음료수냉장고
     public GameObject Panel2;
     //널브러진 과자
     public GameObject Panel3;
 
+    void Start()
+    {
+        AudioSource tvsound = gameObject.GetComponent<AudioSource>();
+    }
     void OnTriggerEnter (Collider col)
     {
         //플레이어가 방문과 충돌하면
@@ -19,37 +25,46 @@ public class TextUI : MonoBehaviour
         {
             //Text UI 활성화
             Panel1.SetActive(true);
-            //2초 후에 비활성화
-            Invoke("False", 2.0f);
-
+            //오디오 재생
+            tvsound.Play();
         }
-        
         //플레이어가 음료수냉장고와 충돌하면
         if (col.gameObject.tag == "DrinkR")
         {
             //Text UI 활성화
             Panel2.SetActive(true);
-            //2초 후에 비활성화
-            Invoke("False", 2.0f);
         }
         //플레이어가 과자와 충돌하면
         if (col.gameObject.tag == "Snack")
         {
             //Text UI 활성화
             Panel3.SetActive(true);
-            //2초 후에 비활성화
-            Invoke("False", 2.0f);
         }
     }
 
-    void False()
+    void OnTriggerExit(Collider col)
     {
-        //Text UI 2초 후에 비활성화시키는 함수
-        Panel1.SetActive(false);
-        //Text UI 2초 후에 비활성화시키는 함수
-        Panel2.SetActive(false);
-        //Text UI 2초 후에 비활성화시키는 함수
-        Panel3.SetActive(false);
+        //플레이어가 방문과 충돌하면
+        if (col.gameObject.tag == "RoomDoor")
+        {
+            //Text UI 2초 후에 비활성화시키는 함수
+            Panel1.SetActive(false);
+            //오디오 종료
+            tvsound.Stop();
+        }
+        //플레이어가 음료수냉장고와 충돌하면
+        if (col.gameObject.tag == "DrinkR")
+        {
+            //Text UI 2초 후에 비활성화시키는 함수
+            Panel2.SetActive(false);
+        }
+        //플레이어가 과자와 충돌하면
+        if (col.gameObject.tag == "Snack")
+        {
+            //Text UI 2초 후에 비활성화시키는 함수
+            Panel3.SetActive(false);
+        }
     }
+
 }
 
