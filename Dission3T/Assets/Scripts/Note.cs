a54f5187e586dade067ad2e8c028820fba92b83b
alskzldud <alskzldud@naver.com>
2022-12-06 20:22:44 +0900

    노트 버튼 구현

---
+++ Dission3T/Assets/Scripts/Note.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Note : MonoBehaviour
+{
+    public GameObject noteOpen;
+
+    void Update()
+    {
+        //노트가 열려있을 때
+        if (noteOpen.activeSelf == true)
+        {
+            //n을 누르면
+            if (Input.GetKeyDown("n"))
+            {
+                //노트 닫기
+                noteOpen.SetActive(false);
+            }
+        }
+        //노트가 닫혀있을 때
+        else
+        {
+            //n을 누르면
+            if (Input.GetKeyDown("n"))
+            {
+                //노트 열기
+                noteOpen.SetActive(true);
+            }
+        }
+    }
+}
